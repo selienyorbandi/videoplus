87c680d9c46041a273d28c7d01f1335496dcf7b4
Selien Yorbandi <selienyorbandi@gmail.com>
2022-07-12 13:18:27 -0300

    Lazy loading for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,103 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "pages/Home/Home";
-import Movies from "pages/Movies/Movies";
-import Series from "pages/Series/Series";
-import Genres from "pages/Genres/Genres";
-import MovieDetail from "pages/MovieDetail/MovieDetail";
-import Search from "pages/Search/Search";
-import Notfound from "pages/not-found";
-
+import Loader from "components/Loader/Loader";
 import Header from "components/Header/Header";
-import Footer from "components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
+  const Home = lazy(() => import("pages/home/Home"));
+  const Movies = lazy(() => import("pages/movies/Movies"));
+  const Series = lazy(() => import("pages/series/Series"));
+  const Genres = lazy(() => import("pages/genres/Genres"));
+  const Search = lazy(() => import("pages/search/Search"));
+  const MovieDetail = lazy(() => import("pages/movieDetail/MovieDetail"));
+  const Notfound = lazy(() => import("pages/not-found"));
+  const Footer = lazy(() => import("components/Footer/Footer"));
+
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <main className="Main">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/peliculas" element={<Movies />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/generos/:type/:id" element={<Genres />} />
-          <Route path="/generos" element={<Genres />} />
-          <Route path="/m/:id" element={<MovieDetail/>} />
-          <Route path="/s/:id" element={<MovieDetail/>} />
-          <Route path="/busqueda/:query" element={<Search/>} />
-          <Route path="*" element={<Notfound />} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/peliculas"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Movies />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/series"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Series />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/generos/:type/:id"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Genres />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/generos"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Genres />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/m/:id"
+            element={
+              <Suspense fallback={<Loader />}>
+                <MovieDetail />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/s/:id"
+            element={
+              <Suspense fallback={<Loader />}>
+                <MovieDetail />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/busqueda/:query"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Search />
+              </Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Notfound />
+              </Suspense>
+            }
+          />
         </Routes>
       </main>
-      <Footer/>
+      <Suspense fallback={null}>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,14 +2,34 @@ import Movie from "components/Movie/Movie";
 
 import styles from "./styles.module.css";
 
-function MovieList({movies, med_type}){
-  return(
+function MovieList({ movies, med_type }) {
+  return (
     <ul className={styles.Movie__list}>
-      {movies.map( eachMovie => {
-        const {name, title, overview, poster_path, vote_average, id, media_type} = eachMovie;
-        return <li key={id}><Movie name={name} title={title} overview={overview} poster_path={poster_path} vote_average={vote_average} key={id} id={id} media_type={media_type || med_type}/></li>;
-      })
-      }          
+      {movies.map((eachMovie) => {
+        const {
+          name,
+          title,
+          overview,
+          poster_path,
+          vote_average,
+          id,
+          media_type,
+        } = eachMovie;
+        return (
+          <li key={id}>
+            <Movie
+              name={name}
+              title={title}
+              overview={overview}
+              poster_path={poster_path}
+              vote_average={vote_average}
+              key={id}
+              id={id}
+              media_type={media_type || med_type}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }
