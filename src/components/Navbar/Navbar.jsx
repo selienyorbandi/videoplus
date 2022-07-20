3ba8e7542fde74091f86feb22ba30461b5311935
Selien Yorbandi <selienyorbandi@gmail.com>
2022-07-20 08:07:09 -0300

    Active className for active pages in Nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import logo from "../../assets/images/logo.png";
 
 import styles from "./styles.module.css";
 
-/* //todo isActive class */
 function Navbar(){
 
   const [isOpen, setIsOpen] = useState(false);
@@ -36,10 +35,10 @@ function Navbar(){
       <nav className={styles.NavBar}>
         <Link to="/"><img src={logo} alt="Videoplus logo" className={styles.NavBar__logoImg} width="225" height="70"/></Link>
         <ul className={`${styles.NavBar__navlist} ${isOpen ? styles.NavBar__navlist__open : styles.NavBar__navlist__closed}`}>
-          <li><NavLink to="/" className={({isActive}) => (isActive ? "active" : "")}>Inicio</NavLink></li>
-          <li><NavLink to="/peliculas" className={({isActive}) => (isActive ? "active" : "")}>Películas</NavLink></li>
-          <li><NavLink to="/series" className={({isActive}) => (isActive ? "active" : "")}>Series</NavLink></li>
-          <li><NavLink to="/generos/m/28" className={({ isActive }) => isActive ? "active" : ""}>Géneros</NavLink></li>
+          <li><NavLink to="/" className={({ isActive }) => isActive ? styles.NavBar__Link__Active : undefined}>Inicio</NavLink></li>
+          <li><NavLink to="/peliculas" className={({ isActive }) => isActive ? styles.NavBar__Link__Active : undefined}>Películas</NavLink></li>
+          <li><NavLink to="/series" className={({ isActive }) => isActive ? styles.NavBar__Link__Active : undefined}>Series</NavLink></li>
+          <li><NavLink to="/generos/m/28" className={({ isActive }) => isActive ? styles.NavBar__Link__Active : undefined}>Géneros</NavLink></li>
           <div><SearchBar/></div>
         </ul>
         <div className={styles.NavBar__searchBar}><SearchBar/></div>
